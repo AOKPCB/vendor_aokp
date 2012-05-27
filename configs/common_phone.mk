a118f90f80edea395cb6aee5612c429256d629d0
remicks <lost.junglist@gmail.com>
2012-05-26 22:42:14 -0500

    Fuck it, why not add Trebuchet too?

--- configs/common_phone.mk
+++ configs/common_phone.mk
@@ -2,6 +2,9 @@ $(call inherit-product, vendor/aokp/configs/common.mk)
 
 PRODUCT_COPY_FILES += \
    vendor/aokp/proprietary/NovaLauncher.apk:system/app/NovaLauncher.apk
+
+PRODUCT_PACKAGES += \
+        Trebuchet
      
     
     
