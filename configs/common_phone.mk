06b26675823479e091403609e2da2b19e441f928
remicks <lost.junglist@gmail.com>
2012-05-26 22:48:16 -0500

    Remove Nova, use Apex here.

--- configs/common_phone.mk
+++ configs/common_phone.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, vendor/aokp/configs/common.mk)
 
 PRODUCT_COPY_FILES += \
-   vendor/aokp/proprietary/NovaLauncher.apk:system/app/NovaLauncher.apk
+    vendor/aokp/proprietary/ApexLauncher.apk:system/app/ApexLauncher.apk
      
     
     
