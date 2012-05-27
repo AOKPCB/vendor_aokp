66bfd7e5c9e6892be1b663b020b18e8dece09a16
remicks <lost.junglist@gmail.com>
2012-05-26 22:46:44 -0500

    Gonna do this the right way

--- configs/common_phone.mk
+++ configs/common_phone.mk
@@ -2,9 +2,6 @@ $(call inherit-product, vendor/aokp/configs/common.mk)
 
 PRODUCT_COPY_FILES += \
    vendor/aokp/proprietary/NovaLauncher.apk:system/app/NovaLauncher.apk
-
-PRODUCT_PACKAGES += \
-        Trebuchet
      
     
     
