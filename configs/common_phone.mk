8ee0218f469c0dadc1278c4698748334f7ecd0c2
remicks <lost.junglist@gmail.com>
2012-05-26 22:41:13 -0500

    Redundancy check complete.

--- configs/common_phone.mk
+++ configs/common_phone.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, vendor/aokp/configs/common.mk)
 
 PRODUCT_COPY_FILES += \
-    vendor/aokp/prebuilt/common/app/NovaLauncher.apk:system/app/NovaLauncher.apk
+   vendor/aokp/proprietary/NovaLauncher.apk:system/app/NovaLauncher.apk
      
     
     
