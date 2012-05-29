69b202ac6f0a5f24803982b468d83bd72259a5eb
remicks <lost.junglist@gmail.com>
2012-05-28 21:45:28 -0500

    no fucking way, typo in the device tree was fuckin it ALL up... DERP!

--- products/stingray.mk
+++ products/stingray.mk
@@ -14,5 +14,4 @@ PRODUCT_MANUFACTURER := Motorola
 PRODUCT_BUILD_PROP_OVERRIDES := PRODUCT_NAME=trygon BUILD_ID=IML74K BUILD_FINGERPRINT=verizon/trygon/stingray:3.2.4/HLK75F/221360:user/release-keys PRIVATE_BUILD_DESC="trygon-user 3.2.4 HLK75F 221360 release-keys" BUILD_NUMBER=221360
 
 PRODUCT_COPY_FILES +=  \
-    vendor/aokp/prebuilt/common/media/bootanimation.zip:system/media/bootanimation.zip \
-    device/sample/etc/apns-conf_verizon.xml:system/etc/apns-conf.xml
+    vendor/aokp/prebuilt/common/media/bootanimation.zip:system/media/bootanimation.zip
